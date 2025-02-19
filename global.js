2d56ae0df7114281662ed0d0acc83a90bf88e507
syw003 <147453782+syw003@users.noreply.github.com>
2025-02-19 14:56:29 -0800

    add base href

--- global.js
+++ global.js
@@ -32,11 +32,11 @@ function $$(selector, context = document) {
 
 let pages = [
     {url: '', title: 'Home'},
-    {url: './projects/', title: 'Projects'},
-    {url: './contact/', title: 'Contact'},
-    {url: './cv/', title: 'CV'},
+    {url: 'projects/', title: 'Projects'},
+    {url: 'contact/', title: 'Contact'},
+    {url: 'cv/', title: 'CV'},
     {url: 'https://github.com/syw003', title: 'Profile'},
-    {url: './meta/', title: 'Meta' }
+    {url: 'meta/', title: 'Meta' }
 ];
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
@@ -48,21 +48,27 @@ document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    console.log(p)
+    // console.log(p)
     url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url: url;
+    // console.log(url)
     
     let a = document.createElement('a');
+    console.log(url)
+
     a.href = url;
     a.textContent = title;
 
-    a.classList.toggle(
-        'current',
-        a.host === location.host && a.pathname === location.pathname
-      );
-    // a.toggleAttribute("target", a.host!==location.host);
-    a.target = a.host !== location.host? "_blank" : "";
-
     nav.append(a);
+
+
+//     a.classList.toggle(
+//         'current',
+//         a.host === location.host && a.pathname === location.pathname
+//       );
+//     a.toggleAttribute("target", a.host!==location.host);
+
+//     a.target = a.host !== location.host? "_blank" : "";
+
 }
 
 
@@ -112,7 +118,7 @@ form?.addEventListener("submit", function (e) {
     for (let [name, value] of data) {
         value = encodeURIComponent(value);
         url += `${name}=${value}&`;
-        console.log(name, value);
+        // console.log(name, value);
       }
       url = url.slice(0, -1);
       location.href = url;
